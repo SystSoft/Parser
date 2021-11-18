c8e109ae021db6c6452c3462b3addb1b68b1e478
an372315 <90437547+an372315@users.noreply.github.com>
2021-11-17 22:16:27 -0500

    Update vm.c

--- vm.c
+++ vm.c
@@ -1,9 +1,3 @@
-/*
-This is the skeleton vm.c for the UCF Fall 2021 Systems Software 
-Project, HW4. Add whatever you want, but make sure to leave the
-print functions as they are to make sure you get your grade as
-quickly as possible. 
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
